9676cadb5d873cfaeb7611dc1608f5c19ec03fdd
Donald L. Merand <dmerand@explo.org>
2016-03-04 16:48:46 -0500

    update setup example

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 echo "NOTE: This is designed to work only on a Raspberry Pi"
+echo "eg 'sudo setup.sh'"
 
 sudo apt-get update
 sudo apt-get install build-essential python-dev python-pip python-smbus git
