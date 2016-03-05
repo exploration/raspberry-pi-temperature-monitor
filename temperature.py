de8bb60d2f100d2a8dfa5c8fb38648ec174ce2f6
Donald L. Merand <dmerand@explo.org>
2016-03-04 21:46:48 -0500

    add logfile rotating to hipchat and clean up the rest dramatically

--- temperature.py
+++ temperature.py
@@ -1,91 +1,103 @@
 #!/usr/bin/env python
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
-# Modified by: Donald Merand for Explo (http://www.explo.org)
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# Author: Donald Merand for Explo (http://www.explo.org)
+# Some bits are from Adafruit (see README)
 
-import logging
-import time, subprocess
+import logging, time, subprocess, os
 import toml
 
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MAX31855.MAX31855 as MAX31855
 
 
+# Define a function to convert celsius to fahrenheit.
+def c_to_f(c):
+  return c * 9.0 / 5.0 + 32.0
 
-# Grab config information from the log file
+
+# Grab config information from the log file (TOML format)
 with open("/home/pi/raspberry-pi-temperature-monitor/config.toml") as conffile:
   conf = toml.loads(conffile.read())
 
-# Define a function to convert celsius to fahrenheit.
-def c_to_f(c):
-        return c * 9.0 / 5.0 + 32.0
 
 # Set up logging if applicable
 LOGFORMAT = "%(asctime)s\t%(message)s"
 if conf.get('log_file'):
-  log_file = conf.get('log_file')
-  log_level = conf.get('log_level')
-  if log_level == 'DEBUG':
-    logging.basicConfig(filename=log_file, format=LOGFORMAT, level=logging.DEBUG)
+  if conf.get('log_level') == 'DEBUG':
+    logging.basicConfig(filename=conf['log_file'], format=LOGFORMAT, level=logging.DEBUG)
   else:
-    logging.basicConfig(filename=log_file, format=LOGFORMAT, level=logging.INFO)
-  print("Logging to {0}".format(log_file))
+    # Default to INFO-level logging
+    logging.basicConfig(filename=conf['log_file'], format=LOGFORMAT, level=logging.INFO)
+  print("Logging to {0}".format(conf['log_file']))
 else:
+  # Just log to STDOUT
   logging.basicConfig(level=logging.INFO, format=LOGFORMAT)
 
-# Set up the poll rate
+
+# Set up the temperature poll rate
 if conf.get('poll_rate'):
   poll_rate = conf.get('poll_rate')
 else:
-  poll_rate = 10 #seconds
+  # Default to 10 seconds if none is set
+  poll_rate = 10
 
-# Uncomment one of the blocks of code below to configure your Pi to use
-# software or hardware SPI.
 
-# Raspberry Pi software SPI configuration.
-sensor = MAX31855.MAX31855(conf['pi']['CLK'], conf['pi']['CS'], conf['pi']['DO'])
-# Raspberry Pi hardware SPI configuration.
-#sensor = MAX31855.MAX31855(spi=SPI.SpiDev(conf['pi']['SPI_PORT'], conf['pi']['SPI_DEVICE']))
+# Set our temperature sensor based on the desired connection method
+if conf.get('pi').get('connect_method') == "HARDWARE":
+  sensor = MAX31855.MAX31855(spi=SPI.SpiDev(conf['pi']['SPI_PORT'], conf['pi']['SPI_DEVICE']))
+else:
+  # Default to software SPI
+  sensor = MAX31855.MAX31855(conf['pi']['CLK'], conf['pi']['CS'], conf['pi']['DO'])
+
 
 # Grab our (wireless) IP address so that we can send a link to the webcam if
-# the temp is too high
+# the temp is too high/low
 ip_command = "ifconfig | awk 'BEGIN{ORS=\"\"}/wlan0/{getline;gsub(/addr:/,\"\",$2);print $2}'"
 my_ip = str(subprocess.check_output(ip_command, shell=True))
 
 
+# We sometimes get false freezing measurements on startup. So we double-check
+# for freezing temps
+might_be_freezing = None
+
 # Loop measurements every second.
 while True:
+  # Rotate log file
+  if conf.get('log_file'):
+    log_size = os.stat(conf.get('log_file')).st_size 
+
+    if conf.get('log_file_rotate_size') == None:
+      log_file_rotate_size = 1000000 #10MB
+    else
+      log_file_rotate_size = conf['log_file_rotate_size']
+
+    if log_size > conf.get('log_file_rotate_size'):
+      # Store log in hipchat
+      if conf.get('hipchat').get('token') and conf.get('hipchat').get('room'):
+        subprocess.call(['/usr/local/bin/hipfile', "--token", conf['hipchat']['token'], "--room", conf['hipchat']['room'], "--path", conf.get('log_file'), "--message", 'Laser Cutter Temperature Log'])
+      # Empty log file
+      with open(conf.get('log_file'), 'w'):
+        pass
+
+
+  # now do the temperature reading
   temp = sensor.readTempC()
   #internal = sensor.readInternalC()
   logging.info("{0:0.3F}*C\t{1:0.3F}*F".format(temp, c_to_f(temp)))
-  #print '    Internal Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(internal, c_to_f(internal))
+
   if temp > conf['temp']['max']:
     warning_text = "WARNING: Laser Cutter is hot, potential fire risk: {0:0.3F}*F. Webcam: http://{1}:8081".format(c_to_f(temp), my_ip)
     logging.warning(warning_text)
     subprocess.call(["/usr/local/bin/hipchat", warning_text])
     time.sleep(10) # wait 10 seconds
   if temp < conf['temp']['min']:
-    warning_text = "WARNING: Laser Cutter is being exposed to freezing temperatures: {0:0.3F}*F. Webcam: http://{1}:8081".format(c_to_f(temp), my_ip)
-    logging.warning(warning_text)
-    subprocess.call(["/usr/local/bin/hipchat", warning_text])
-    time.sleep(60 * 5) # wait 5 minutes
+    if might_be_freezing:
+      might_be_freezing = None
+      warning_text = "WARNING: Laser Cutter is being exposed to freezing temperatures: {0:0.3F}*F. Webcam: http://{1}:8081".format(c_to_f(temp), my_ip)
+      logging.warning(warning_text)
+      subprocess.call(["/usr/local/bin/hipchat", warning_text])
+    else:
+      might_be_freezing = True
+    time.sleep(60 * 5) # wait 5 more minutes to be sure
   else:
-    time.sleep(poll_rate) # normally check every 1 seconds
+    # Temperature is in normal range, wait regular polling amount + try again
+    time.sleep(poll_rate)
